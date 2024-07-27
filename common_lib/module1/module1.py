1d7cbad263337827e9a497a53068469fa553a833
paloma810 <mcyk49129@gmail.com>
2024-07-27 14:46:08 +0900

    ci test

---
+++ common_lib/module1/module1.py
@@ -0,0 +1,2 @@
+def module1():
+    print("module1")
--- common_lib/setup.py
+++ common_lib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test',
+    name='module1',
     version='0.1',
     packages=find_packages()
 )
