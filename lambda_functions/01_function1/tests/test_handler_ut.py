93639e4c734f0314c1437d1292b35c4cb3dd6c1b
paloma810 <mcyk49129@gmail.com>
2024-07-30 22:45:25 +0900

    ci test

--- lambda_functions/01_function1/tests/test_handler_ut.py
+++ lambda_functions/01_function1/tests/test_handler_ut.py
@@ -1,4 +1,9 @@
 # test_lambda_function.py
+import sys
+import os
+
+# handler.pyのディレクトリをsys.pathに追加
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 from handler import lambda_handler
 
 
